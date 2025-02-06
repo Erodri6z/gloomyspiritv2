929da5485f2e0152842653c223c208833e782416
Erodri6z <erikrodriguez2417@gmail.com>
2025-02-05 20:50:23 -0600

    add bottle icons to landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,3 +1,9 @@
+import Whiskey from "./../../assets/bottles/Whiskey.png"
+import Vodka from "../../assets/bottles/Vodka.png"
+import Rum from "../../assets/bottles/Rum.png"
+import Tequila from "../../assets/bottles/Tequila.png"
+import Gin from "../../assets/bottles/Gin.png"
+import Other from "../../assets/bottles/Other.png"
 
 
 const Landing = () => {
@@ -6,8 +12,12 @@ const Landing = () => {
     <div>
       <h3>Welcome to Gloomy Spirits</h3>
       <div class="bottle-interface">
-        <p>images here</p>
-        {/* <img src="" alt="" /> */}
+        <img src={Whiskey} alt="whiskey" />
+        <img src={Vodka} alt="whiskey" />
+        <img src={Rum} alt="whiskey" />
+        <img src={Gin} alt="whiskey" />
+        <img src={Tequila} alt="whiskey" />
+        <img src={Other} alt="whiskey" />
       </div>
     </div>
     </>
