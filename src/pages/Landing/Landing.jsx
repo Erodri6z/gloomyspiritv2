1b900599406fdf6e6e5deff28e15aff86be1c194
Erodri6z <erikrodriguez2417@gmail.com>
2025-02-05 21:07:47 -0600

    fix images

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,6 +4,7 @@ import Rum from "../../assets/bottles/Rum.png"
 import Tequila from "../../assets/bottles/Tequila.png"
 import Gin from "../../assets/bottles/Gin.png"
 import Other from "../../assets/bottles/Other.png"
+import "./Landing.css"
 
 
 const Landing = () => {
@@ -12,12 +13,12 @@ const Landing = () => {
     <div>
       <h3>Welcome to Gloomy Spirits</h3>
       <div class="bottle-interface">
-        <img src={Whiskey} alt="whiskey" />
-        <img src={Vodka} alt="whiskey" />
-        <img src={Rum} alt="whiskey" />
-        <img src={Gin} alt="whiskey" />
-        <img src={Tequila} alt="whiskey" />
-        <img src={Other} alt="whiskey" />
+        <img class="bottle" src={Whiskey} alt="whiskey" />
+        <img class="bottle" src={Vodka} alt="whiskey" />
+        <img class="bottle" src={Rum} alt="whiskey" />
+        <img class="bottle" src={Gin} alt="whiskey" />
+        <img class="bottle" src={Tequila} alt="whiskey" />
+        <img class="bottle" src={Other} alt="whiskey" />
       </div>
     </div>
     </>
