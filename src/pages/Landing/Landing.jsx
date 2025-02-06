12824da56f4c66d80c1c17c1c3931b8f3414ed1a
Erodri6z <erikrodriguez2417@gmail.com>
2025-02-05 20:35:15 -0600

    add a landing stub up

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,15 @@
+const Landing = () => {
+  return (
+    <>
+    <div>
+      <h3>Welcome to Gloomy Spirits</h3>
+      <div class="bottle-interface">
+        <p>images here</p>
+        {/* <img src="" alt="" /> */}
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Landing
