13e8d608a3706825f301084264f647c550f23585
Erodri6z <erikrodriguez2417@gmail.com>
2025-02-05 20:42:09 -0600

    add landing to landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'\
+import { Route, Routes} from 'react-router';
 import Nav from './components/Nav/nav'
+import Landing from './pages/Landing/Landing'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
     <Nav />
-    <div>
-      
-    </div>
+    <Routes>
+      <Route 
+      path="/"
+      element={
+        <Landing />
+      }
+      />
+    </Routes>
     </>
   )
 }
