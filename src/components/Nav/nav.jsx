8c6ba920525b2a1b3c256aaba9e39866b837d19f
Erodri6z <erikrodriguez2417@gmail.com>
2025-02-15 16:36:16 -0600

    add a link to the nav for spirits page

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -7,7 +7,11 @@ function Nav () {
         Gloomy Spirits
       </h3>
       <ul>
-        <li className='nav-item'>Spirits</li>
+        <li className='nav-item'>
+          <a href="/spirits">
+            Spirits
+          </a>
+        </li>
         <li className='nav-item'>Methods</li>
         <li className='nav-item'>
           <a href="/socials">
