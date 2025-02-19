9c9593a4bc2234cdd75dd7d71abf5862e764de8c
Erodri6z <erikrodriguez2417@gmail.com>
2025-02-18 21:19:54 -0600

    add popups

---
+++ src/components/DrinkShow/DrinkShow.jsx
@@ -0,0 +1,47 @@
+const DrinkShow = () => {
+  return (
+    <>
+    <div>
+      <h3>cocktail name</h3>
+      <h4>Main Spiit</h4>
+      <ul>
+        <li>Water</li>
+        <li>sugar</li>
+        <li>lemons</li>
+      </ul>
+      <ul>
+        <li>3oz</li>
+        <li>3oz</li>
+        <li>3oz</li>
+      </ul>
+      <ul>
+        <li>bitters</li>
+      </ul>
+      <ul>
+        <li>Shaken</li>
+        <li>Stirred</li>
+      </ul>
+      <ul>
+        <li> garnish Lemon Slice</li>
+      </ul>
+      <ul>
+        <li>Notes</li>
+      </ul>
+
+      <ul>
+        <li>recommendedGlasses</li>
+      </ul>
+
+      <ul>
+        <li>Notes</li>
+        <li>Vibes</li>
+        <li>credit</li>
+      </ul>
+
+    </div>
+    </>
+  )
+}
+
+
+export default DrinkShow
--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react"
+import Popup from "reactjs-popup"
+// import 'reactjs-popup/dist/index.css'
+import DrinkShow from "../DrinkShow/DrinkShow"
 import Collins from "./../../assets/defaultcups/Collins Glass.png"
 import Coupe from "./../../assets/defaultcups/Coupe Glass.png"
 import Marg from "./../../assets/defaultcups/Margarita Glass.png"
@@ -5,6 +9,8 @@ import Martini from "./../../assets/defaultcups/Martini Glass.png"
 import Rocks from "./../../assets/defaultcups/Rocks Glass.png"
 import "./Results.css"
 const Results = (props) => {
+  // console.log(selectedDrink)
+
   // const fakeDrinks = [
   //   {
   //     name: "Martini",
@@ -45,10 +51,26 @@ const Results = (props) => {
     <>
     <div className="results-div">
       {props.drinks.map(d => 
-        <div key={d.name} className="drink-card" >
+      <Popup trigger={
+
+        <div key={d.id} className="drink-card">
           <img src={d.image? d.image: getImage(d.recommendedGlasses)} alt="drink" style={{ backgroundColor : d.color }} className="thumbnail"/>
           <h3 className="drink-name">{d.name}</h3>
         </div>
+        
+        }
+        margin="auto"
+        contentStyle={{
+        position: "",
+        margin: "auto",
+        width: '300px',
+        padding: '20px',
+        backgroundColor: 'black',
+        textAlign: 'center',
+        arrowStyle: "none"
+        }}>
+        <DrinkShow />
+        </Popup>
       )}
     </div>
     </>
