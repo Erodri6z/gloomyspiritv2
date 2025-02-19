1725db2959c14bd6205b10d273eaf06f4c604f2e
Erodri6z <erikrodriguez2417@gmail.com>
2025-02-18 22:39:54 -0600

    add a way to fix the popup to the center of the page

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import Popup from "reactjs-popup"
-// import 'reactjs-popup/dist/index.css'
+import 'reactjs-popup/dist/index.css'
 import DrinkShow from "../DrinkShow/DrinkShow"
 import Collins from "./../../assets/defaultcups/Collins Glass.png"
 import Coupe from "./../../assets/defaultcups/Coupe Glass.png"
@@ -59,15 +59,22 @@ const Results = (props) => {
         </div>
         
         }
-        margin="auto"
+        // margin="auto"
+        modal
+        closeOnDocumentClick
         contentStyle={{
-        position: "",
-        margin: "auto",
-        width: '300px',
-        padding: '20px',
-        backgroundColor: 'black',
-        textAlign: 'center',
-        arrowStyle: "none"
+          background: "black",
+          padding: "10px",
+          borderRadius: "10px",
+          boxShadow: "0 5px 15px rgba(0, 0, 0, 0.3)",
+          textAlign: "center",
+          maxWidth: "400px",
+          width: "90%", 
+          position: "fixed", 
+          top: "50%", 
+          left: "50%",         
+          transform: "translate(-50%, -50%)", 
+          zIndex: "9999", 
         }}>
         <DrinkShow />
         </Popup>
